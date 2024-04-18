d587d8bcab0c7bd13652aed2c04e78b078e6162c
vilequarter <44734839+vilequarter@users.noreply.github.com>
2024-04-18 14:11:22 +0300

    exercise 3.12

---
+++ mongo.js
@@ -0,0 +1,51 @@
+const mongoose = require('mongoose');
+
+if(process.argv.length < 3){
+    console.log('missing argument ([password] [name?] [number?]');
+    process.exit(1);
+}
+
+const password = process.argv[2];
+const name = process.argv[3];
+const number = process.argv[4];
+
+const db = 'phonebookApp';
+
+const url = `mongodb+srv://vilequarter:${password}@cluster0.thnxixa.mongodb.net/${db}?retryWrites=true&w=majority&appName=Cluster0`;
+
+mongoose.set('strictQuery', false);
+
+mongoose.connect(url);
+
+const personSchema = new mongoose.Schema({
+    name: String,
+    number: String
+})
+
+const Person = mongoose.model('Person', personSchema);
+
+if(name && number){
+    //add person to db
+    const newPerson = new Person({
+        name: process.argv[3],
+        number: process.argv[4]
+    })
+    newPerson.save().then(result => {
+        console.log(`added ${newPerson.name} number ${newPerson.number} to phonebook`);
+        mongoose.connection.close();
+    })
+}else if(!(name || number)){
+    console.log('phonebook:');
+    Person.find({}).then(result => {
+        result.forEach(person => {
+            console.log(`${person.name} ${person.number}`)
+        })
+        mongoose.connection.close();
+    })
+} else{
+    console.log(`missing name or number`);
+    process.exit(1);
+}
+
+
+
